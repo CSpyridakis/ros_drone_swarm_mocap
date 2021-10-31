445d88b6f5baba19f6ea0bbbd95d0c95f9d15be6
Spyridakis Christos <spyridakischristos@gmail.com>
2021-10-31 11:49:24 +0200

    rosbag on launch file added for testing + fix setupEnv script

--- setupEnv.sh
+++ setupEnv.sh
@@ -10,7 +10,6 @@ download_imu_package(){
     (cd ~/catkin_ws/src/ && git clone https://github.com/CSpyridakis/ros-adafruit-10dof-imu.git)
 }
 
-
 setup_uart(){
     # 1. Add ‘enable_uart=1’ to /boot/config.txt file
     sudo bash -c ’echo "enable_uart=1" >> /boot/config.txt’
@@ -45,3 +44,52 @@ install_ros_packages(){
     __INST robot-localization
     __INST image-transport
 }
+
+doAll(){
+    download_imu_package
+    setup_i2c
+    setup_uart
+    install_ros_packages
+}
+
+help_menu(){
+    echo "Usage: $0 [Option] [Option] ..."
+    echo "Setup your Ubuntu environment in order to run this ROS package"
+    echo 
+    echo "Options:"
+    echo "  -h,  --help                 show this help page"
+    echo "  -i,  --imu-package          download needed IMU ROS package from GitHub"
+    echo "  -p,  --ros-packages         install all ROS requirement packages from APT"
+    echo "  -U,  --setup-uart           setup UART to read messages from there"
+    echo "  -I,  --setup-i2c            download needed lib and setup I2C to read messages from there"
+    echo "  -A,  --all                  do all above actions"
+}
+
+#MAIN
+while :
+do
+    case "$1" in
+        -i | --imu-package)     download_imu_package    ; shift ;;
+        -p | --ros-packages)    install_ros_packages    ; shift ;;
+
+        -U | --setup-uart)      setup_uart              ; shift ;;  
+        -I | --setup-i2c)       setup_i2c               ; shift ;;
+
+        -A | --all)             doAll                   ; exit 0 ;;
+
+        -h | --help)            help_menu  ; exit 0 ;;
+        
+        --*)
+            echo "Unknown option: $1" >&2
+            helpMenu
+            exit 1
+            ;;
+        -*)
+            echo "Unknown option: $1" >&2
+            helpMenu
+            exit 1 
+            ;;
+        *) 
+            break
+    esac
+done
