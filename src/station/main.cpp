58caf75312720e908de2ea8e13dadb330ff666b2
Spyridakis Christos <spyridakischristos@gmail.com>
2022-02-26 10:54:56 +0200

    Broadcast estimated pose to network

--- src/station/main.cpp
+++ src/station/main.cpp
@@ -13,8 +13,25 @@
 
 #include "ros_drone_swarm_mocap/mocap_worker_data.h"
 
+#include <geometry_msgs/Pose.h>
+#include <geometry_msgs/PoseStamped.h>
+
 using namespace ros_drone_swarm_mocap;
 using namespace message_filters;
+
+ros::Publisher posePub;
+
+geometry_msgs::Pose setPose(double x, double y, double z, double ox, double oy, double oz, double ow){
+    geometry_msgs::Pose pose;
+    pose.position.x = x; 
+    pose.position.y = y;
+    pose.position.z = z; 
+    pose.orientation.x = ox; 
+    pose.orientation.y = oy; 
+    pose.orientation.z = oz; 
+    pose.orientation.w = ow;
+    return pose;
+}
 
 void localizeObjectCB(const ros_drone_swarm_mocap::mocap_worker_data::ConstPtr& procData1, 
                       const ros_drone_swarm_mocap::mocap_worker_data::ConstPtr& procData2, 
@@ -38,6 +55,9 @@ void localizeObjectCB(const ros_drone_swarm_mocap::mocap_worker_data::ConstPtr& 
             
     trilateration(anchors, objectsPose);
 
+    geometry_msgs::Pose pose = setPose(objectsPose.pos.x, objectsPose.pos.y, objectsPose.pos.z, objectsPose.ori.roll, objectsPose.ori.pitch, objectsPose.ori.yaw, 0.0);
+    posePub.publish(pose);
+
     ROS_INFO("B (%f, %f, %f)\n", objectsPose.pos.x, objectsPose.pos.y, objectsPose.pos.z);
 }
 
@@ -45,6 +65,8 @@ int packet_id = 0;
 int main(int argc, char **argv){
     ros::init(argc, argv, "ball_localization");
     ros::NodeHandle n;
+
+    posePub = n.advertise<geometry_msgs::Pose>("/master/obj_pose", 1);
 
     message_filters::Subscriber<ros_drone_swarm_mocap::mocap_worker_data> worker_1(n, "/node/1/processed_data", 1);
     message_filters::Subscriber<ros_drone_swarm_mocap::mocap_worker_data> worker_2(n, "/node/2/processed_data", 1);
